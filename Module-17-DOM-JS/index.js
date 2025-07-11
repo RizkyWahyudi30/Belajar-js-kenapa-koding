19c00497d433775134acff5fb2f8b9baaa1e0ebb
Rizky Wahyudi <rizkywahyudi302008@gmail.com>
2025-07-11 17:48:33 +0700

    new knowladge

---
+++ Module-17-DOM-JS/index.js
@@ -0,0 +1,7 @@
+// DOM 
+// DOM adalah representasi struktur dokumen HTML atau XML dalam bentuk tree yang memunngkinkan program untuk mengakses dan memanipulasi konten, struktur dan gaya halaman web secara dinamis 
+
+// di DOM ada dua penjelasan, yaitu: 
+// 1. Node: setiap elemen dalam tree DOM disebur node. Ada beberapa jenis node, ada elemen, teks, atribut
+// 2. Root node: dokumen HTML dimulai dari Node akar(document), yang merupakan entry point untuk mengakses semua elemen lain di halaman 
+
