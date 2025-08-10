1c75999aeac8f114bbbc7edc05603d2ae965d502
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-10 17:02:13 +0700

    upgrade code

---
+++ Project/calculator-simple/script.js
@@ -0,0 +1,31 @@
+function countNumber() {
+  let firstNumber = Number(document.getElementById("numberOne").value);
+  let secondNumber = Number(document.getElementById("numberTwo").value);
+  let operator = document.getElementById("operator").value;
+  let result;
+
+  if (isNaN(firstNumber) || isNaN(secondNumber) || operator === "") {
+    document.getElementById("result").innerText =
+      "Tolong pilih angka dan operator yang valid!";
+  } else if (operator === "+") {
+    result = firstNumber + secondNumber;
+    document.getElementById(
+      "result"
+    ).innerText = `Hasil angka pertama: ${firstNumber} dan angka kedua: ${secondNumber}. Ditambah = ${result}`;
+  } else if (operator === "-") {
+    result = firstNumber - secondNumber;
+    document.getElementById(
+      "result"
+    ).innerText = `Hasil angka pertama: ${firstNumber} dan angka kedua: ${secondNumber}. Dikurang = ${result}`;
+  } else if (operator === "*") {
+    result = firstNumber * secondNumber;
+    document.getElementById(
+      "result"
+    ).innerText = `Hasil angka pertama: ${firstNumber} dan angka kedua: ${secondNumber}. Dikali = ${result}`;
+  } else if (operator === "/") {
+    result = firstNumber / secondNumber;
+    document.getElementById(
+      "result"
+    ).innerText = `Hasil angka pertama: ${firstNumber} dan angka kedua: ${secondNumber}. Dibagi = ${result}`;
+  }
+}
