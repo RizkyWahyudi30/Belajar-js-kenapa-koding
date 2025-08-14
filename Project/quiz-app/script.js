2f566aa962e6082289e8f074f3ec5aff3a64b242
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-14 17:32:09 +0700

    new code

---
+++ Project/quiz-app/script.js
@@ -0,0 +1,19 @@
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
+const questionText = document.getElementById("question-text");
+const totalQuestions = document.getElementById("total-question");
+const score = document.getElementById("score");
+const answerQuiz = document.getElementById("answer-quiz");
+const progressBar = document.getElementById("progress");
+const resultScreen = document.getElementById("result-screen");
+const finalScoreQuiz = document.getElementById("final-score");
+const maxScoreQuiz = document.getElementById("max-score");
+const resultMessage = document.getElementById("result-message");
+const resultButton = document.getElementById("restart-btn");
+
+// questtion of quiz :
+const quizQuestion = [
+  {
+    question: "What ",
+  },
+];
