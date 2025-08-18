04e1c202c6e86f5171bccbeded41f87d981c4fa7
rizkywahyudi <rizkywahyudi302008@gmail.com>
2025-08-18 11:56:33 +0700

    learning cisco packet tracer

--- Project/soal-gpt/day-1/variable-data-type.js
+++ Project/soal-gpt/day-1/variable-data-type.js
@@ -1,5 +1,3 @@
-let listHobbies = [];
-
 function inputUser() {
   const name = document.getElementById("nameUser").value;
   const age = document.getElementById("ageUser").value;
@@ -9,18 +7,16 @@ function inputUser() {
   if (name === "" || age === "" || hobbies === "") {
     alert("Please fill in all fields");
   } else {
-    if (hobbies.trim() !== "") {
-      listHobbies.push(hobbies.trim());
+    if (hobbies) {
       const li = document.createElement("li");
-      li.innerText = hobbies.trim();
-      document.getElementById("hobbyUser").value = "";
+      li.innerText = hobbies;
+      document.getElementById("hobbyList").appendChild(li);
     }
 
-    document.getElementById("resultUser").innerHTML = `
+    document.getElementById("resultUser").innerText = `
     Nama: ${name}, 
     Umur: ${age}, 
-    Hobi: 
-    ${listHobbies.map((h, i) => `${i + 1}. ${h}`).join("\n")}
+    Hobbies:
     `;
   }
 }
@@ -30,5 +26,53 @@ function clearResult() {
   document.getElementById("nameUser").value = "";
   document.getElementById("ageUser").value = "";
   document.getElementById("hobbyUser").value = "";
-  listHobbies = [];
+  document.getElementById("hobbyList").innerText = "";
+}
+
+// ================================================================================== //
+
+let price = [];
+
+function dataPrice() {
+  const changeType = document.getElementById("priceData").value;
+  // validasi if input empty
+  if (changeType === "") {
+    alert("Please input a price");
+    return;
+  }
+
+  const data = Number(changeType);
+
+  if (isNaN(data)) {
+    alert("Please enter a valid number");
+  } else {
+    if (price.length === 1 && price[0] === 0) {
+      price = [];
+    }
+
+    price.push(data);
+    document.getElementById("resultPrice").innerText = `Data price ${price.join(
+      ", "
+    )}`;
+
+    const total = price.reduce((acc, curr) => acc + curr, 0);
+    const average = total / price.length;
+
+    // with spread operator
+    const priceMax = Math.max(...price);
+    const priceMin = Math.min(...price);
+
+    document.getElementById("resultPrice").innerText += `
+    Total: ${total},
+    Average: ${average},
+    Price Max: ${priceMax},
+    Price Min: ${priceMin}
+    `;
+  }
+}
+
+function resetData() {
+  document.getElementById("resultPrice").innerText = "";
+  document.getElementById("priceData").value = "";
+  price = [];
 }
